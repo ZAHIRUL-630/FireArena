e613accf6c4bede2f394ce09c6ce4d5959d29cd2
MUHAMMAD ZAHIRUL <jibonsarker228@gmail.com>
2025-06-28 16:27:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
   const link = document.createElement("a");
-  link.href = "FireArenaX.apk";
+  link.href = "FireArena BD.apk";
   link.download = "FireArenaX.apk";
   document.body.appendChild(link);
   link.click();
